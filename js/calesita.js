b2873f2fff0de537c72b71b8d327df570178a2de
Fsoffian <fsoffiantini@gmail.com>
2022-05-29 19:52:50 -0300

    agregado de swipe para movil

--- js/calesita.js
+++ js/calesita.js
@@ -1,36 +1,55 @@
 const buttons = document.querySelectorAll("[data-carrousel-button]");
 const flechitaDer = document.querySelector('.carrousel-button.next');
 const flechitaIzq = document.querySelector('.carrousel-button.prev');
-/* const repTexto = document.querySelector */
+const carrouselTodo = document.querySelector('.carrousel');
+
 buttons.forEach(button=> {
     button.addEventListener("click", () => {
-        const offset = button.dataset.carrouselButton === "next" ? 1 : -1
+        const offset = button.dataset.carrouselButton === "next" ? 1 : -1;
         const slides = button
             .closest("[data-carrousel]")
             .querySelector("[data-slides]")
         
         
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
+        const activeSlide = slides.querySelector("[data-active]");
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
     })
 })
 
 
-
-
-if (window.innerWidth < 450) {
+let isMobile = window.matchMedia("(pointer:coarse)").matches;
+if (isMobile){
     flechitaDer.innerText = '>';
     flechitaIzq.innerText = '<';
 }
 
-let isMobile = window.matchMedia("(pointer:coarse)").matches;
-if (isMobile){
-    console.log("Movil!")
-}
-else{
-    console.log("noMovil")
-}
+/*!
+ * swiped-events.js - v1.1.6
+ * Pure JavaScript swipe events
+ * https://github.com/john-doherty/swiped-events
+ * @inspiration https://stackoverflow.com/questions/16348031/disable-scrolling-when-touch-moving-certain-element
+ * @author John Doherty <www.johndoherty.info>
+ * @license MIT
+ */
+!function(t,e){"use strict";"function"!=typeof t.CustomEvent&&(t.CustomEvent=function(t,n){n=n||{bubbles:!1,cancelable:!1,detail:void 0};var a=e.createEvent("CustomEvent");return a.initCustomEvent(t,n.bubbles,n.cancelable,n.detail),a},t.CustomEvent.prototype=t.Event.prototype),e.addEventListener("touchstart",function(t){if("true"===t.target.getAttribute("data-swipe-ignore"))return;s=t.target,r=Date.now(),n=t.touches[0].clientX,a=t.touches[0].clientY,u=0,i=0},!1),e.addEventListener("touchmove",function(t){if(!n||!a)return;var e=t.touches[0].clientX,r=t.touches[0].clientY;u=n-e,i=a-r},!1),e.addEventListener("touchend",function(t){if(s!==t.target)return;var e=parseInt(l(s,"data-swipe-threshold","20"),10),o=parseInt(l(s,"data-swipe-timeout","500"),10),c=Date.now()-r,d="",p=t.changedTouches||t.touches||[];Math.abs(u)>Math.abs(i)?Math.abs(u)>e&&c<o&&(d=u>0?"swiped-left":"swiped-right"):Math.abs(i)>e&&c<o&&(d=i>0?"swiped-up":"swiped-down");if(""!==d){var b={dir:d.replace(/swiped-/,""),touchType:(p[0]||{}).touchType||"direct",xStart:parseInt(n,10),xEnd:parseInt((p[0]||{}).clientX||-1,10),yStart:parseInt(a,10),yEnd:parseInt((p[0]||{}).clientY||-1,10)};s.dispatchEvent(new CustomEvent("swiped",{bubbles:!0,cancelable:!0,detail:b})),s.dispatchEvent(new CustomEvent(d,{bubbles:!0,cancelable:!0,detail:b}))}n=null,a=null,r=null},!1);var n=null,a=null,u=null,i=null,r=null,s=null;function l(t,n,a){for(;t&&t!==e.documentElement;){var u=t.getAttribute(n);if(u)return u;t=t.parentNode}return a}}(window,document);
+
+
+
+  carrouselTodo.addEventListener('swiped', function(e) {
+    const offset = (e.detail.dir) === "swiped-right" ? 1 : -1;
+    const slides = carrouselTodo
+        .closest("[data-carrousel]")
+        .querySelector("[data-slides]")
+    
+    
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  });
